62304ee2f58cb6d890d01d78bd12e9163d1035fa
NormalMood <littlebigdiamond@gmail.com>
2022-12-24 13:26:57 +0300

    add gpio and exit handler

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-import os
 from flask import Flask, render_template, request
+import OPi.GPIO as GPIO
+
+#IN1 - purple - forward, IN2 - green - backward, ENB - blue - speed, servo - orange - rotation
+forward_pin = 3 #PA12
+backward_pin = 5 #PA11
+speed_pin = 7 #PA6
+servo_pin = 11 #PA1
+dc = 30
+hz = 25
+pwm = GPIO.PWM(speed_pin, hz)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(forward_pin, GPIO.OUT)
+GPIO.setup(backward_pin, GPIO.OUT)
+GPIO.setup(speed_pin, GPIO.OUT)
+GPIO.setup(servo_pin, GPIO.OUT)
+#GPIO.output(forward_pin, 1)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bc83989cdfdd894859fkdlfd83i489ffjdj99'
@@ -8,6 +24,7 @@ current_user = ""
 
 @app.route('/')
 def index():
+
     global current_user
 
     if current_user == "":
@@ -21,6 +38,9 @@ def forward_start():
     global current_user
     if current_user == request.environ['REMOTE_ADDR']:
         print("forward start")
+        GPIO.output(forward_pin, 1)
+        GPIO.output(backward_pin, 0)
+        #pwm.start(dc)
     return "nothing"
 
 
@@ -29,6 +49,9 @@ def forward_stop():
     global current_user
     if current_user == request.environ['REMOTE_ADDR']:
         print("forward stop")
+        GPIO.output(forward_pin, 0)
+        GPIO.output(backward_pin, 0)
+        #pwm.stop()
     return "nothing"
 
 
@@ -37,6 +60,7 @@ def left_start():
     global current_user
     if current_user == request.environ['REMOTE_ADDR']:   
         print ("left start")
+
     return "nothing"
 
 
@@ -69,6 +93,9 @@ def backward_start():
     global current_user
     if current_user == request.environ['REMOTE_ADDR']:
         print ("backward start")
+        GPIO.output(forward_pin, 0)
+        GPIO.output(backward_pin, 1)
+        #pwm.start(dc)
     return "nothing"
 
 
@@ -77,8 +104,17 @@ def backward_stop():
     global current_user
     if current_user == request.environ['REMOTE_ADDR']:
         print ("backward stop")
+        GPIO.output(forward_pin, 0)
+        GPIO.output(backward_pin, 0)
+        #pwm.stop()
     return "nothing"
 
+@app.route('/exit')
+def exit():
+    global current_user
+    current_user = ""
+    print("exit")
+    return "nothing"
 
 
 if __name__ == "__main__":
